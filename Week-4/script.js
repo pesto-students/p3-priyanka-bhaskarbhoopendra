29155e7e0cc42e5b5fd995ca6988b08a6067d2cc
bhoopendra <bhaskarbhoopendra8@gmail.com>
2022-07-08 13:14:33 +0530

    initial commit

--- Week-4/script.js
+++ Week-4/script.js
@@ -1,109 +1,12 @@
-const STATE = {
-  FULLFILLED: "fullfilled",
-  REJECTED: "rejected",
-  PENDING: "pending",
+const Person = function () {};
+
+Person.prototype.initialize = function (name, age) {
+  this.name = name;
+  this.age = age;
 };
 
-class MyPromise {
-  #thenCbs = [];
-  #catchCbs = [];
-  #state = STATE.PENDING;
-  #value;
-  #onSuccessBind = this.#onSucess.bind(this);
-  #OnFailBind = this.#onFail.bind(this);
+//TODO create the class teacher and a method teach
 
-  constructor(cb) {
-    try {
-      cb(this.#onSuccessBind, this.#OnFailBind);
-    } catch (error) {
-      // any time promise fails it rejects with error
-      this.onFail(error);
-    }
-  }
+var him = new Teacher();
 
-  #runCallbacks() {
-    if (this.#state === STATE.FULLFILLED) {
-      this.#thenCbs.forEach((callback) => {
-        callback(this.#value);
-      });
-      this.#thenCbs = [];
-    }
-
-    if (this.#state === STATE.REJECTED) {
-      this.#catchCbs.forEach((callback) => {
-        callback(this.#value);
-      });
-    }
-    this.#catchCbs = [];
-  }
-
-  #onSucess(value) {
-    queueMicrotask(() => {
-      if (this.#state !== STATE.PENDING) return;
-
-      if (value instanceof MyPromise) {
-        value.then(this.#onSuccessBind, this.#onFailBind);
-        return;
-      }
-
-      this.#value = value;
-      this.#state = STATE.FULFILLED;
-      this.#runCallbacks();
-    });
-  }
-
-  #onFail(value) {
-    queueMicrotask(() => {
-      if (this.#state !== STATE.PENDING) return;
-      if (value instanceof MyPromise) {
-        value.then(this.#onSuccessBind, this.#onFailBind);
-        return;
-      }
-      if (this.#catchCbs.length === 0) {
-        throw new UncaughtPromiseError(value);
-      }
-      this.#value = value;
-      this.#state = STATE.REJECTED;
-      this.#runCallbacks();
-    });
-  }
-
-  then(thenCb, catchCb) {
-    return new MyPromise((resolve, reject) => {
-      this.#thenCbs.push((result) => {
-        if (thenCb == null) {
-          resolve(result);
-          return;
-        }
-        try {
-          resolve(thenCb(result));
-        } catch (error) {
-          reject(error);
-        }
-      });
-
-      this.#catchCbs.push((result) => {
-        if (catchCb == null) {
-          reject(result);
-          return;
-        }
-        try {
-          resolve(catchcb(result));
-        } catch (error) {
-          reject(error);
-        }
-      });
-
-      this.#runCallbacks();
-    });
-  }
-
-  catch(cb) {
-    this.then(undefined, cb);
-  }
-}
-
-export default MyPromise;
-
-new Promise((resolve, reject) => {});
-new Promise(cb);
+him.initialize("Adam", 45);
