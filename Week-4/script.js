3f80330b6b9aabd603b92be590fb034d9147ea78
bhoopendra <bhaskarbhoopendra8@gmail.com>
2022-07-13 19:41:19 +0530

    adding console log as output

--- Week-4/script.js
+++ Week-4/script.js
@@ -8,13 +8,14 @@ Person.prototype.initialize = function (name, age) {
 
 //TODO create the class teacher and a method teach
 function Teacher() {
-  this.teach = (value) => {
-    console.log(value);
+  this.teach = (subject) => {
+    this.subject = subject;
+    console.log(`${this.name} teaches ${this.subject}`);
   };
 }
 
 Object.setPrototypeOf(Teacher.prototype, Person.prototype);
 
 var him = new Teacher();
-him.initialize("adam", 15);
-him.teach("Adam");
+him.initialize("Adam", 45);
+him.teach("Physics");
